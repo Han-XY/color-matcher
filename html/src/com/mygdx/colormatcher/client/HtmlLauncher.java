b3d495152994070578e3a727f09744f2d4070bda
Han Xuanyuan <xuanyuanhan@gmail.com>
2017-07-27 15:14:02 +0100

    Initial commit

---
+++ html/src/com/mygdx/colormatcher/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mygdx.colormatcher.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.colormatcher.ColorMatcher;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new ColorMatcher();
+        }
+}
